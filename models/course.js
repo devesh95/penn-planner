2583d7c9281f0c8091f42501fc8af0a10950d57b
Sumit <ssumit@seas.upenn.edu>
2017-01-03 23:59:02 +0530

    fetching all reviews

--- models/course.js
+++ models/course.js
@@ -1,13 +1,33 @@
-var PCR = require("pcr")
+var PCR = require("pcr");
+var async = require("async");
 
 pcr = new PCR("Az_oxUJPPtlMvauTEsmN3YefCPYk_U");
 
 courseReviewsByName = function getReviewsByName(name, cb) {
-	pcr.courseHistory(name, function(err, result) {
+	
+	var tokens = name.trim().split(' ');
+	var dept = tokens[0];
+	var coursecode = tokens[1];
+	var section;
+	if (tokens.length > 2) {
+		section = tokens[2];
+	}
+
+	pcr.department(dept, function(err, courses) {
 		if (!err) {
-			cb(null, result);
-		} else {
-			cb(err, null);
+			courses.result.coursehistories.forEach(function(course) {
+				course.aliases.forEach(function(alias) { 
+					if (alias.endsWith(coursecode)) {
+						pcr.courseHistoryReviews(course.id, function(err, data) {
+							if (!err) {
+								data.result.values.forEach(function(review) {
+									// Process each review here
+								})
+							}
+						});
+					}
+				});
+			});
 		}
 	});
 }; 
--- models/test.js
+++ models/test.js
@@ -1,6 +1,6 @@
 var course = require('../models/course')
 
-course.courseReviewsByName('CIS-121', function(err, result) {
-	console.log(result.result.courses);
+course.courseReviewsByName('CIS 121', function(err, result) {
+	console.log(result);
 
 });
