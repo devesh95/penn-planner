0dcf4705b445eb59d89c261af74ea52c1f92a92c
Devesh Dayal <devesh95@gmail.com>
2017-01-04 12:29:28 +0530

    new test file for pcr module

--- models/test.js
+++ models/test.js
@@ -1,6 +1,25 @@
 var course = require('../models/course')
 
-course.courseReviewsByName('WRIT 039 302', function(err, result) {
-	console.log(result);
 
-});
+/*
+      Test routine for the PCR wrapper.
+ */
+
+
+var testRoutine = function(coursename) {
+  console.log('-----------------------\nStarting test for ' + coursename + '\n');
+  var start = Date.now();
+  course.courseReviewsByName(coursename, function(err, result) {
+    console.log(err, result);
+    console.log('Time taken: ' + (Date.now() - start) + 'ms.\n');
+
+    // this should be faster because of the cache
+    start = Date.now();
+    course.courseReviewsByName(coursename, function(err, result) {
+      console.log(err, result);
+      console.log('Time taken: ' + (Date.now() - start) + 'ms. (Should be much faster than above)\n');
+    });
+  });
+}
+
+testRoutine('CIS 197');
